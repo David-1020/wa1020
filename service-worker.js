1a32a6f3905575e628211b098b1520fbf8cfb299
David-1020 <dc521763@gmail.com>
2024-10-20 22:17:16 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'product-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/latest.json',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
