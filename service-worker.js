475e285c5b9d1996800336652f2f47552abacbe9
David-1020 <dc521763@gmail.com>
2024-10-20 22:35:00 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = 'product-app-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/latest.json',
-  '/manifest.json'
+  '/wa1020/',  // 根目錄指向專案的 GitHub Pages 路徑
+  '/wa1020/index.html',
+  '/wa1020/latest.json',
+  '/wa1020/manifest.json',
+  '/wa1020/icon-192x192.png',
+  '/wa1020/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
